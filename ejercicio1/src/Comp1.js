7c79d9016aa68c2662cb22282bf0ac479a6cf224
Duyer508 <157503412+Duyer508@users.noreply.github.com>
2024-06-04 19:38:10 +0000

    CAMBIO

--- ejercicio1/src/Comp1.js
+++ ejercicio1/src/Comp1.js
@@ -4,7 +4,7 @@ function  Uno(){
     return(
         <div className="caja1">
             <div className="caja2">
-                <h1  className="titulos">COMPONENTE PRUEBA</h1>
+                <h1  className="titulos">EJERCICIO 8</h1>
                 <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
                 <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
                 <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
