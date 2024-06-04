77b582bae133f69154d9bb73c15c13d275a8b9a6
Duyer508 <157503412+Duyer508@users.noreply.github.com>
2024-06-04 19:30:01 +0000

    cambio

---
+++ ejercicio1/src/Comp1.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+
+function  Uno(){
+    return(
+        <div className="caja1">
+            <div className="caja2">
+                <h1  className="titulos">COMPONENTE PRUEBA</h1>
+                <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
+                <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
+                <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
+                <p className="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur commodi sunt inventore perspiciatis quia delectus numquam? Ipsum cupiditate itaque accusantium autem quia enim consequatur aut, nemo deserunt optio nulla ut.</p>
+
+            </div>
+            <div className="caja3">
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+        </div>
+    );
+}
+export default Uno; 
